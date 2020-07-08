abba206fe5efa74a849ea46e69a64703dc688a74
Enrico lerrigatto La Cava <me@lerrigatto.com>
2020-07-08 11:49:07 +0200

    es0707 matrix with same line

--- src/es07-bt/es0707.py
+++ src/es07-bt/es0707.py
@@ -1,17 +1,19 @@
 # Dato n, si stampino tutte le matrici n x n di valori {a,b,c} tali che
 # i simboli in ciascuna riga siano tutti uguali
 
+def print_matr(M):
+    for line in M:
+        print(''.join(line))
+    print("---")
 
-def stampa_naive():
-    # Si generano tutte le matrici possibili
-    # Si stampano solo quelle che rispettano il criterio
+def stampa_bt(n, i=0, M=[]):
+    if i == n:
+        print_matr(M)
+        return
+    for x in ['a','b','c']:
+        T = [x] * n
+        M.append(T)
+        stampa_bt(n,i+1,M)
+        M.pop()
 
-    return
-
-def stampa_bt():
-    return
-
-def main():
-    stampa_naive(n)
-    stampa_bt(n)
-main()
+stampa_bt(3)
