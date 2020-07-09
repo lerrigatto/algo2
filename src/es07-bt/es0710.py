fe218fa81dc6eabb620c5af595b0e4fd71ae296c
Enrico lerrigatto La Cava <me@lerrigatto.com>
2020-07-09 14:58:26 +0200

    es0710 diagonal checks, incomplete

--- src/es07-bt/es0710.py
+++ src/es07-bt/es0710.py
@@ -12,52 +12,44 @@ def print_matr(M):
 def magic_sq(n, M, T, S, i=0, j=0):
     lsum = (n*(n*n+1))/2
     if i == n:
+        print(S)
         print_matr(M)
         return
     for v in range(1,n*n+1):
         if not T[v]:
             diag = None
             diag_ok = True
-            #if i==j:
-            #    diag = 2*n + 1
-            #if i+j == n:
-            #    diag = 2*n + 2
-            #if diag:
-            #    if S[diag]+v <= lsum:
-            #        diag_ok = True
-            #    else:
-            #        diag_ok = False
-            #else:
-            #    diag_ok = True
-            #    diag = None
-            print(f"i{i},j{j},v{v},diag{diag} :{diag_ok}")
-            #if diag_ok and \
-            if  S[i]+v <= lsum and \
-                S[n+j]+v <= lsum:
-                    S[i] = S[i] + v
-                    S[n+j] = S[n+j] + v
-                    #if diag_ok and diag:
-                    #    S[diag] = S[diag] + v
-                    M[i][j] = v
-                    T[v] = True
-                    print(M)
-                    if j < n-1:
-                        # Continua sulla riga
-                        magic_sq(n,M,T,S,i,j+1)
-                    else:
-                        magic_sq(n,M,T,S,i+1)
-                    T[v] = False
-                    M[i][j] = 0
-                    S[i] = S[i] - v
-                    S[n+j] = S[n+j] - v
-                    #if diag_ok and diag:
-                    #    S[diag] = S[diag] - v
-                    #    diag = None
+            if i==j:
+                diag = 2*n
+            if i+j == n:
+                diag = 2*n + 1
+            if diag and S[diag]+v > lsum:
+                diag_ok = False
 
+            if diag_ok and \
+                S[i]+v <= lsum and \
+                S[n+j]+v <=lsum:
 
+                if diag_ok and diag:
+                    S[diag] = S[diag] + v
+                S[i] = S[i] + v
+                S[n+j] = S[n+j] + v
+                M[i][j] = v
+                T[v] = True
+                if j < n-1:
+                    # Continua sulla riga
+                    magic_sq(n,M,T,S,i,j+1)
+                else:
+                    magic_sq(n,M,T,S,i+1)
+                T[v] = False
+                M[i][j] = 0
+                S[i] = S[i] - v
+                S[n+j] = S[n+j] - v
+                if diag_ok and diag:
+                    S[diag] = S[diag] - v
 
 n=3
 M = [[0] * n for _ in range(n)]
 T = [False] * int(n*n+1)
-S = [0] * int(2*n +3)
+S = [0] * int(2*n +2)
 magic_sq(n, M, T, S)
